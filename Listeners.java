b7c13b1dca0596db4a1e78eada81d0d3de957c6c
XelaPi <vanyoalex@gmail.com>
2013-04-21 12:16:48 -0500

    Create Listeners.java

---
+++ Listeners.java
@@ -0,0 +1,89 @@
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+/**
+ * 
+ * This class handles all listeners (user input). As the program is intended to be a screensaver, when any user input is detected the program is exited.
+ * 
+ * @author Alex
+ *
+ */
+public class Listeners implements MouseListener, MouseMotionListener, MouseWheelListener, KeyListener {
+  
+	// Private variable that stores the last place the mouse was. If it changes, the program is exited.
+	private Point previousPoint = new Point(-1, -1);
+	
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		if (!Main.ONLY_EXIT_ON_KEY) {
+			Main.close();
+		}
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		if (!Main.ONLY_EXIT_ON_KEY) {
+			if (previousPoint.x == -1) {
+				// If previousPoint has a -1 value as its x value (The first time the function is called), previousPoint is to be where the mouse is.
+				previousPoint = e.getPoint();
+			} else {
+				// Otherwise, the mouse has moved, and the program should quit.
+				if (Math.hypot(previousPoint.x - e.getPoint().x, previousPoint.y - e.getPoint().y) > 1) {
+					Main.close();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (!Main.ONLY_EXIT_ON_KEY) {
+			Main.close();
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if (!Main.ONLY_EXIT_ON_KEY) {
+			Main.close();
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == Main.EXIT_KEY || !Main.ONLY_EXIT_ON_KEY) {
+			Main.close();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (!Main.ONLY_EXIT_ON_KEY) {
+			Main.close();
+		}
+	}
+
+}
